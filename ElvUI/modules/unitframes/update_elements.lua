d13dffadb601f4c341837a47d4c44fead5828255
Catok <anloren@gmail.com>
2012-04-27 12:32:40 +0200

    Show "spellsteal border" for hunter and lock. More changes later.

--- ElvUI/modules/unitframes/update_elements.lua
+++ ElvUI/modules/unitframes/update_elements.lua
@@ -370,7 +370,7 @@ function UF:PostUpdateAura(unit, button, index, offset, filter, isDebuff, durati
 			button.icon:SetDesaturated(false)
 		end
 	else
-		if (button.isStealable or ((E.myclass == "PRIEST" or E.myclass == "SHAMAN" or E.myclass == "MAGE") and dtype == "Magic")) and not UnitIsFriend("player", unit) then
+		if (button.isStealable or ((E.myclass == "PRIEST" or E.myclass == "SHAMAN" or E.myclass == "MAGE" or E.myclass == "HUNTER" or E.myclass == "WARLOCK") and dtype == "Magic")) and not UnitIsFriend("player", unit) then
 			button:SetBackdropBorderColor(237/255, 234/255, 142/255)
 		else
 			button:SetBackdropBorderColor(unpack(E["media"].bordercolor))		
